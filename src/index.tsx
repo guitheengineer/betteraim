ec488b2bcb74eeec34a34b45e135e63726820a3c
Guilherme Samuel <guilhermesnd14@gmail.com>
2022-02-28 23:39:12 -0300

    refactor: remove unused webvitalsreport import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'context';
 import { StrictMode } from 'react';
 
@@ -12,8 +11,3 @@ ReactDOM.render(
   </StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
